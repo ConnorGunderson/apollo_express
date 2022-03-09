0b19b5c49982011881ae7731b91032f0c8638ef3
Connor Gunderson <gundydev@gmail.com>
2022-03-09 00:59:38 -0500

    init folder structure

--- server/src/graphql/main.ts
+++ server/src/graphql/main.ts
@@ -1,18 +1,23 @@
 import { ApolloServer, Config, ExpressContext } from 'apollo-server-express'
 import express from 'express'
 import { schema } from './nexus'
+require('dotenv').config()
 
-export const startServer = (
+export const startServer = async (
   typeDefs?: Config<ExpressContext>['typeDefs'],
   resolvers?: Config<ExpressContext>['resolvers']
 ) => {
   const app = express()
   const server = new ApolloServer({ typeDefs, resolvers, schema })
+  await server.start()
+
   server.applyMiddleware({ app })
+
+  app.use(express.urlencoded({ extended: true }))
 
   app.listen({ port: process.env.PORT }, () => {
     console.log(
-      `Server currently listening on port ${process.env.PORT} ${server.graphqlPath}`
+      `Server currently listening \nPORT: ${process.env.PORT} \nGraphQL: ${server.graphqlPath}`
     )
   })
 }
---
+++ server/src/index.ts
@@ -0,0 +1,3 @@
+import { startServer } from './graphql/main'
+
+startServer()
